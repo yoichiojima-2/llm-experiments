3f7d4b3f88cbad592279b50232b9eaadaab52a85
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-21 21:47:14 +0900

    style: apply ruff & isort

--- llm_experiments/rabbit.py
+++ llm_experiments/rabbit.py
@@ -1,11 +1,13 @@
 import typing as t
 from argparse import ArgumentParser, Namespace
+from pprint import pprint
+
 from langchain.chat_models import init_chat_model
 from langchain_community.tools import ShellTool
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
-from langchain_core.messages import HumanMessage
 
 
 class Agent:
@@ -16,18 +18,24 @@ class Agent:
         search = TavilySearchResults(max_results=2)
         shell = ShellTool()
         tools = [search, shell]
-        self.agent = create_react_agent(model.bind_tools(tools), tools, checkpointer=memory)
+        self.agent = create_react_agent(
+            model.bind_tools(tools), tools, checkpointer=memory
+        )
 
     def stream(self, query: str):
         messages = [HumanMessage(content=query)]
-        for step, metadata in self.agent.stream({"messages": messages}, self.config, stream_mode="messages"):
-            from pprint import pprint; pprint(step)
+        for step, metadata in self.agent.stream(
+            {"messages": messages}, self.config, stream_mode="messages"
+        ):
+            pprint(step)
             if metadata["langgraph_node"] == "agent" and (text := step.text()):
                 yield text
 
     def invoke(self, query: str):
         messages = [HumanMessage(content=query)]
-        for step in self.agent.invoke({"messages": messages}, self.config, stream_mode="values"):
+        for step in self.agent.invoke(
+            {"messages": messages}, self.config, stream_mode="values"
+        ):
             return step
 
 
