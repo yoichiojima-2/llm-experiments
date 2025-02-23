eeb9c1c465e593ec924a5f23cd779310e5499828
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-24 00:58:08 +0900

    fix: chat_models.py -> chat.py

--- llm_experiments/chat.py
+++ llm_experiments/chat.py
@@ -1,12 +1,11 @@
 from langchain.chat_models import init_chat_model
-from langchain_ollama.llms import OllamaLLM
 
 
-def instantiate_chat_model(opt: str):
+def instantiate_chat(opt: str):
     match opt:
         case "gpt-4o-mini":
             return init_chat_model("gpt-4o-mini", model_provider="openai")
         case "deepseek-r1:8b":
-            return OllamaLLM(model="deepseek-r1:8b")
+            return init_chat_model("deepseek-r1:8b", model_provider="ollama")
         case _:
             raise ValueError(f"Unknown model: {opt}")
