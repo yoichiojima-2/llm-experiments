af3b29fdc997977fb6113696884984c7291a63da
Yoichi Ojima <yoichiojima@gmail.com>
2025-02-24 15:24:01 +0900

    test: fix test_chat

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -2,21 +2,21 @@ from llm_experiments import chat
 
 
 def test_4o_mini():
-    model = chat.instantiate_chat("gpt-4o-mini")
+    model = chat.instantiate_chat("4o-mini")
     res = model.invoke("test").content
     print(res)
     assert res
 
 
 def test_r1():
-    model = chat.instantiate_chat("deepseek-r1:8b")
+    model = chat.instantiate_chat("deepseek")
     res = model.invoke("test")
     print(res)
     assert res
 
 
 def test_llama():
-    model = chat.instantiate_chat("llama3.2")
+    model = chat.instantiate_chat("llama")
     res = model.invoke("test")
     print(res)
     assert res
