37858ab7bd95ab6a87dbb887ae52744956b05ab4
yoichi ojima <yoichiojima@gmail.com>
2025-03-30 00:05:40 +0900

    wip: git_commit.py

--- bin/git_commit.py
+++ bin/git_commit.py
@@ -1,4 +1,5 @@
 import sys
+import textwrap
 
 from langgraph.prebuilt import create_react_agent
 
@@ -6,11 +7,19 @@ from llm_experiments import tools
 from llm_experiments.llm import create_model
 
 
+
+
 def main():
     try:
+        prompt = textwrap.dedent(
+            """
+            your task is create git commit with concise and readable commit messages with given shell tool.
+            inspect git diff and git add by lines, then git commit repeatedly to avoid making too large or multi-purpose commits.
+            """
+        )
         model = create_model("gpt-o3-mini")
         agent = create_react_agent(model, tools.shell())
-        res = agent.invoke({"messaages": "execute git diff and create commit message"})
+        res = agent.invoke({"messages": prompt})
 
         for i in res["messages"]:
             print(i.content)
