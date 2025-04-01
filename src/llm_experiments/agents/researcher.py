25bfdb98783b9a819d5d480143980b9e40120bf8
yoichi ojima <yoichiojima@gmail.com>
2025-04-01 22:55:45 +0900

    fix: use prompts module in researcher agent

--- src/llm_experiments/agents/researcher.py
+++ src/llm_experiments/agents/researcher.py
@@ -1,10 +1,10 @@
 from argparse import ArgumentParser
 from dotenv import load_dotenv
 
-from langchain import hub
 from langchain.agents import AgentExecutor, create_react_agent
 
 from llm_experiments.llm import create_model
+from llm_experiments import prompts
 from llm_experiments import tools
 
 
@@ -12,21 +12,20 @@ def parse_args():
     parser = ArgumentParser()
     opt = parser.add_argument
     opt("--query", "-q", type=str, required=True)
-    opt("--model", "-m", type=str, default="gpt-4o-mini")
+    opt("--model", "-m", type=str, default="4o-mini")
     opt("--verbose", "-v", action="store_true", default=False)
     return parser.parse_args()
 
 
 def researcher(query: str, model_opt: str, verbose=False) -> None:
     model = create_model(model_opt)
-    prompt = hub.pull("hwchase17/react")
-    tool_list = [
-        tools.duckduckgo(),
-        tools.wikipedia(),
-        tools.tavily(),
-    ]
+    prompt = prompts.multipurpose()
+
+    tool_list = [tools.duckduckgo(), tools.wikipedia(), tools.tavily()]
+
     agent = create_react_agent(model, tool_list, prompt=prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tool_list, handle_parsing_errors=True, verbose=verbose)
+
     res = agent_executor.stream({"input": query})
     for i in res:
         print(i["messages"][-1].content, end="\n\n")
