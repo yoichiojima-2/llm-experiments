a51a0f3c4f681629b1f850e96c519af32ce12204
yoichi ojima <yoichiojima@gmail.com>
2025-04-08 22:42:29 +0900

    refactor: slack tools

--- src/llm_experiments/slack_tools.py
+++ src/llm_experiments/slack_tools.py
@@ -41,46 +41,46 @@ class SlackTools:
     def post_message(self, channel: str, text: str) -> tuple[bool, str, str]:
         res = self.client.chat_postMessage(channel=channel, text=text)
         print(f"message sent to {channel}: {text}")
-        return res["ok"], res["channel"], res["ts"]
+        return res
 
     @handle_error
     def post_ephemeral(self, channel: str, text: str, user: str) -> tuple[bool, str, str]:
         res = self.client.chat_postEphemeral(channel=channel, text=text, user=user)
         print(f"ephemeral message sent to {channel}: {text}")
-        return res["ok"], res["channel"], res["ts"]
+        return res
 
     @handle_error
     def update_message(self, channel: str, ts: str, text: str) -> tuple[bool, str, str]:
         res = self.client.chat_update(channel=channel, ts=ts, text=text)
         print(f"message updated in {channel}: {text}")
-        return res["ok"], res["channel"], res["ts"]
+        return res
 
     @handle_error
     def delete_message(self, channel: str, ts: str) -> tuple[bool, str, str]:
         res = self.client.chat_delete(channel=channel, ts=ts)
         print(f"message deleted in {channel}: {ts}")
-        return res["ok"], res["channel"], res["ts"]
+        return res
 
     @handle_error
     def add_reaction(self, channel: str, emoji_name: str, ts: str) -> tuple[bool, str, str]:
         res = self.client.reactions_add(channel=channel, name=emoji_name, timestamp=ts)
         print(f"reaction added to {channel}: {emoji_name} at {ts}")
-        return res["ok"], res["channel"], res["ts"]
+        return res
 
     @handle_error
     def remove_reaction(self, channel, emoji_name: str, ts: str) -> tuple[bool, str, str]:
         res = self.client.reactions_remove(channel=channel, name=emoji_name, timestamp=ts)
         print(f"reaction removed from {channel}: {emoji_name} at {ts}")
-        return res["ok"], res["channel"], res["ts"]
+        return res
 
     @handle_error
     def upload_file(self, channels: str, file: str) -> tuple[bool, str, str]:
         res = self.client.files_upload_v2(channels=channels, file=file)
         print(f"file uploaded to {channels}: {file}")
-        return res["ok"], res["file"]["id"], res["file"]["name"]
+        return res
 
     @handle_error
     def add_remote_file(self, channels: list[str], file: str) -> tuple[bool, str, str]:
         res = self.client.files_remote_add(channels=channels, file=file)
         print(f"remote file added to {channels}: {file}")
-        return res["ok"], res["file"]["id"], res["file"]["name"]
+        return res
--- tests/test_slack_tools.py
+++ tests/test_slack_tools.py
@@ -4,20 +4,18 @@ from llm_experiments.tools import make_tools_by_name
 
 def test_slack_tools():
     slack_tools = SlackTools()
-    status, channel, ts = slack_tools.post_message("test", "hello from agent")
-    assert status
+    res = slack_tools.post_message("test", "hello from agent")
+    assert res["ok"]
 
-    print(f"status: {status}, channel: {channel}, ts: {ts}")
-    status, channel, ts = slack_tools.update_message(channel, ts, "hello from agent updated")
-    assert status
+    res = slack_tools.update_message(res["channel"], res["ts"], "hello from agent updated")
+    assert res["ok"]
 
-    print(f"status: {status}, channel: {channel}, ts: {ts}")
-    status, channel, ts = slack_tools.delete_message(channel, ts)
-    assert status
+    res = slack_tools.delete_message(res["channel"], res["ts"])
+    assert res["ok"]
 
     tool_by_name = make_tools_by_name(slack_tools.tools)
-    status, channel, ts = tool_by_name["post"].invoke({"channel": "test", "text": "hello from agent"})
-    assert status
+    res = tool_by_name["post"].invoke({"channel": "test", "text": "hello from agent"})
+    assert res["ok"]
 
-    status, _, _ = tool_by_name["delete"].invoke({"channel": channel, "ts": ts})
-    assert status
+    res = tool_by_name["delete"].invoke({"channel": res["channel"], "ts": res["ts"]})
+    assert res["ok"]
