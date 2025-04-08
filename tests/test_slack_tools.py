654b10e6f454fb453fc09f51185c7da7aa0c803b
yoichi ojima <yoichiojima@gmail.com>
2025-04-08 21:45:37 +0900

    test: add test_slack_tools

--- tests/test_slack_tools.py
+++ tests/test_slack_tools.py
@@ -1,3 +1,4 @@
+from llm_experiments.tools import make_tools_by_name
 from llm_experiments.slack_tools import SlackTools
 
 
@@ -5,10 +6,18 @@ def test_slack_tools():
     slack_tools = SlackTools()
     status, channel, ts = slack_tools.post_message("test", "hello from agent")
     assert status
+
     print(f"status: {status}, channel: {channel}, ts: {ts}")
     status, channel, ts = slack_tools.update_message(channel, ts, "hello from agent updated")
     assert status
+
     print(f"status: {status}, channel: {channel}, ts: {ts}")
     status, channel, ts = slack_tools.delete_message(channel, ts)
     assert status
-    print(f"status: {status}, channel: {channel}, ts: {ts}")
+
+    tool_by_name = make_tools_by_name(slack_tools.tools)
+    status, channel, ts = tool_by_name["post"].invoke({"channel": "test", "text": "hello from agent"})
+    assert status
+
+    status, _, _ = tool_by_name["delete"].invoke({"channel": channel, "ts": ts})
+    assert status
