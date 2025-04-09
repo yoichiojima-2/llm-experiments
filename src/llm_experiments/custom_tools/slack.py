e73199c8dd671f42b8c39ca61442da6fa6f81ee6
yoichi ojima <yoichiojima@gmail.com>
2025-04-09 22:50:25 +0900

    fix: slack_tools

--- src/llm_experiments/custom_tools/slack.py
+++ src/llm_experiments/custom_tools/slack.py
@@ -1,22 +1,7 @@
 import os
-import sys
-from functools import wraps
 
 import slack_sdk
 from langchain_core.tools import tool
-from slack_sdk.errors import SlackApiError
-
-
-def handle_error(func):
-    @wraps(func)
-    def wrapper(*a, **kw):
-        try:
-            return func(*a, **kw)
-        except SlackApiError as e:
-            print(f"error: {e.response['error']}", file=sys.stderr)
-            return None
-
-    return wrapper
 
 
 class SlackTools:
@@ -28,46 +13,52 @@ class SlackTools:
         @tool
         def post_message(channel: str, text: str):
             """post a message to a Slack channel."""
-            return self.post_message(channel, text)
+            return self.client.chat_postMessage(channel=channel, text=text)
 
         @tool
         def delete_message(channel: str, ts: str):
             """delete a message from a Slack channel."""
-            return self.delete_message(channel, ts)
+            return self.client.chat_delete(channel=channel, ts=ts)
+
+        @tool
+        def post_ephemeral(channel: str, text: str, user: str):
+            """post an ephemeral message to a Slack channel."""
+            return self.client.chat_postEphemeral(channel=channel, text=text, user=user)
 
         @tool
         def update_message(channel: str, ts: str, text: str):
             """update a message in a Slack channel."""
-            return self.update_message(channel, ts, text)
+            return self.client.chat_update(channel=channel, ts=ts, text=text)
 
         @tool
         def add_reaction(channel: str, emoji_name: str, ts: str):
             """add a reaction to a message in a Slack channel."""
-            return self.add_reaction(channel, emoji_name, ts)
+            return self.client.reactions_add(channel=channel, name=emoji_name, timestamp=ts)
 
         @tool
         def remove_reaction(channel: str, emoji_name: str, ts: str):
             """remove a reaction from a message in a Slack channel."""
-            return self.remove_reaction(channel, emoji_name, ts)
+            return self.client.reactions_remove(channel=channel, name=emoji_name, timestamp=ts)
 
         @tool
         def upload_file(channels: str, file: str):
             """upload a file to a Slack channel."""
-            return self.upload_file(channels, file)
+            return self.client.files_upload_v2(channels=channels, file=file)
 
         @tool
         def add_remote_file(channels: list[str], file: str):
             """add a remote file to a Slack channel."""
-            return self.add_remote_file(channels, file)
+            return self.client.files_remote_add(channels=channels, file=file)
 
         @tool
         def list_conversations():
             """list all conversations in Slack."""
-            return self.list_conversations()
+            return self.client.conversations_list()
 
         return [
             post_message,
             delete_message,
+            post_ephemeral,
             update_message,
             add_reaction,
             remove_reaction,
@@ -75,39 +66,3 @@ class SlackTools:
             add_remote_file,
             list_conversations,
         ]
-
-    @handle_error
-    def post_message(self, channel: str, text: str):
-        return self.client.chat_postMessage(channel=channel, text=text)
-
-    @handle_error
-    def post_ephemeral(self, channel: str, text: str, user: str):
-        return self.client.chat_postEphemeral(channel=channel, text=text, user=user)
-
-    @handle_error
-    def update_message(self, channel: str, ts: str, text: str):
-        return self.client.chat_update(channel=channel, ts=ts, text=text)
-
-    @handle_error
-    def delete_message(self, channel: str, ts: str):
-        return self.client.chat_delete(channel=channel, ts=ts)
-
-    @handle_error
-    def add_reaction(self, channel: str, emoji_name: str, ts: str):
-        return self.client.reactions_add(channel=channel, name=emoji_name, timestamp=ts)
-
-    @handle_error
-    def remove_reaction(self, channel, emoji_name: str, ts: str):
-        return self.client.reactions_remove(channel=channel, name=emoji_name, timestamp=ts)
-
-    @handle_error
-    def upload_file(self, channels: str, file: str):
-        return self.client.files_upload_v2(channels=channels, file=file)
-
-    @handle_error
-    def add_remote_file(self, channels: list[str], file: str):
-        return self.client.files_remote_add(channels=channels, file=file)
-
-    @handle_error
-    def list_conversations(self):
-        return self.client.conversations_list()
