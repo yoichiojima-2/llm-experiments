ec30902e2b834901cbcca554eb44a63cd95f0537
yoichi ojima <yoichiojima@gmail.com>
2025-04-29 17:57:17 +0900

    fix: standardise tools interfaces

--- src/llm_experiments/tools.py
+++ src/llm_experiments/tools.py
@@ -7,65 +7,62 @@ from langchain_community.tools import DuckDuckGoSearchRun, ShellTool, WikipediaQ
 from langchain_community.utilities import GoogleSerperAPIWrapper, WikipediaAPIWrapper
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_core.tools import tool
+from langchain_core.tools.base import BaseTool
 from langchain_experimental.utilities import PythonREPL
 from langchain_tavily import TavilySearch
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
-from langchain_core.tools.base import BaseTool
 
 from llm_experiments.custom_tools.slack import SlackToolkit
 
 
-
-# singuler tools
-def duckduckgo() -> BaseTool:
-    return DuckDuckGoSearchRun()
+def duckduckgo() -> list[BaseTool]:
+    return [DuckDuckGoSearchRun()]
 
 
-def shell(*a, **kw) -> BaseTool:
-    return ShellTool(*a, **kw)
+def shell(*a, **kw) -> list[BaseTool]:
+    return [ShellTool(*a, **kw)]
 
 
-def python_repl() -> BaseTool:
+def python_repl() -> list[BaseTool]:
     @tool
     def python_repl_tool(script):
         """python repr run"""
         return PythonREPL().run(script)
 
-    return python_repl_tool
+    return [python_repl_tool]
 
 
-def wikipedia() -> BaseTool:
-    return WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+def wikipedia() -> list[BaseTool]:
+    return [WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())]
 
 
-def serper() -> BaseTool:
+def serper() -> list[BaseTool]:
     @tool
     def serper_tool(query):
         """serper search"""
         return GoogleSerperAPIWrapper().run(query)
 
-    return serper_tool
+    return [serper_tool]
 
 
-def tavily() -> BaseTool:
-    return TavilySearch(max_results=5)
+def tavily() -> list[BaseTool]:
+    return [TavilySearch(max_results=5)]
 
 
-# toolkits
-async def browser_tools(browser) -> list[BaseTool]:
+async def browser(browser) -> list[BaseTool]:
     return PlayWrightBrowserToolkit.from_browser(async_browser=browser).get_tools()
 
 
-def slack_tools() -> list[BaseTool]:
+def slack() -> list[BaseTool]:
     return SlackToolkit().get_tools()
 
 
-def file_management_tools(*a, **kw) -> list[BaseTool]:
+def file_management(*a, **kw) -> list[BaseTool]:
     return FileManagementToolkit(*a, **kw).get_tools()
 
 
-def sql_tools(model, db_name) -> BaseTool:
+def sql(model, db_name) -> list[BaseTool]:
     db_dir = Path(__file__).parent.parent.parent / "db"
     path = db_dir / f"{db_name}.db"
     path.parent.mkdir(parents=True, exist_ok=True)
