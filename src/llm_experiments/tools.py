91e04b9ab979d8b3aaf78850476eb960bb23f417
yoichi ojima <yoichiojima@gmail.com>
2025-05-02 14:19:09 +0900

    fix: all tools inherit BaseToolkit

--- src/llm_experiments/tools.py
+++ src/llm_experiments/tools.py
@@ -1,82 +1,194 @@
+import os
 import sqlite3
 from pathlib import Path
+from abc import ABC
 
+import slack_sdk
 from langchain_community.agent_toolkits import FileManagementToolkit, PlayWrightBrowserToolkit
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_community.tools import DuckDuckGoSearchRun, ShellTool, WikipediaQueryRun
 from langchain_community.utilities import GoogleSerperAPIWrapper, WikipediaAPIWrapper
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_core.tools import tool
-from langchain_core.tools.base import BaseTool
+from langchain_core.tools.base import BaseTool, BaseToolkit
 from langchain_experimental.utilities import PythonREPL
 from langchain_tavily import TavilySearch
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
 
-from llm_experiments.custom_tools.slack import SlackToolkit
+
+class Tools(BaseToolkit, ABC):
+    def get_tools_by_name(self) -> dict[str, BaseTool]:
+        return {tool.name: tool for tool in self.get_tools()}
+    
+
+class Slack(Tools):
+    @property
+    def client(self):
+        return slack_sdk.WebClient(token=os.getenv("SLACK_USER_TOKEN"))
+
+    def get_tools(self) -> list[BaseTool]:
+        @tool
+        def post_message(channel: str, text: str):
+            """post a message to a Slack channel."""
+            return self.client.chat_postMessage(channel=channel, text=text)
+
+        @tool
+        def delete_message(channel: str, ts: str):
+            """delete a message from a Slack channel."""
+            return self.client.chat_delete(channel=channel, ts=ts)
+
+        @tool
+        def post_ephemeral(channel: str, text: str, user: str):
+            """post an ephemeral message to a Slack channel."""
+            return self.client.chat_postEphemeral(channel=channel, text=text, user=user)
+
+        @tool
+        def update_message(channel: str, ts: str, text: str):
+            """update a message in a Slack channel."""
+            return self.client.chat_update(channel=channel, ts=ts, text=text)
+
+        @tool
+        def add_reaction(channel: str, emoji_name: str, ts: str):
+            """add a reaction to a message in a Slack channel."""
+            return self.client.reactions_add(channel=channel, name=emoji_name, timestamp=ts)
+
+        @tool
+        def remove_reaction(channel: str, emoji_name: str, ts: str):
+            """remove a reaction from a message in a Slack channel."""
+            return self.client.reactions_remove(channel=channel, name=emoji_name, timestamp=ts)
+
+        @tool
+        def upload_file(channels: str, file: str):
+            """upload a file to a Slack channel."""
+            return self.client.files_upload_v2(channels=channels, file=file)
+
+        @tool
+        def add_remote_file(channels: list[str], file: str):
+            """add a remote file to a Slack channel."""
+            return self.client.files_remote_add(channels=channels, file=file)
+
+        @tool
+        def list_conversations(limit: int = 100):
+            """list all conversations in Slack."""
+            return self.client.conversations_list(limit=limit)
+
+        @tool
+        def get_conversation_history(channel: str):
+            """get the history of a conversation in Slack."""
+            return self.client.conversations_history(channel=channel)
+
+        @tool
+        def start_direct_message(users: list[str]):
+            """open a conversation in Slack."""
+            return self.client.conversations_open(users=users)
+
+        @tool
+        def create_channel(name: str):
+            """create a new channel in Slack."""
+            return self.client.conversations_archive(name=name)
+
+        @tool
+        def get_conversation_info(channel: str):
+            """get information about a conversation in Slack."""
+            return self.client.conversations_info(channel=channel)
+
+        @tool
+        def get_members_of_conversation(channel: str):
+            """get members of a conversation in Slack."""
+            return self.client.conversations_members(channel=channel)
+
+        @tool
+        def join_conversation(channel: str):
+            """join a conversation in Slack."""
+            return self.client.conversations_join(channel=channel)
+
+        @tool
+        def leave_conversation(channel: str):
+            """leave a conversation in Slack."""
+            return self.client.conversations_leave(channel=channel)
+
+        return [
+            post_message,
+            delete_message,
+            post_ephemeral,
+            update_message,
+            add_reaction,
+            remove_reaction,
+            upload_file,
+            add_remote_file,
+            list_conversations,
+            get_conversation_history,
+            start_direct_message,
+            create_channel,
+            get_conversation_info,
+            get_members_of_conversation,
+            join_conversation,
+            leave_conversation,
+        ]
 
 
-def duckduckgo() -> list[BaseTool]:
-    return [DuckDuckGoSearchRun()]
+class DuckDuckGo(Tools):
+    def get_tools(self) -> list[BaseTool]:
+        return [DuckDuckGoSearchRun()]
 
 
-def shell(*a, **kw) -> list[BaseTool]:
-    return [ShellTool(*a, **kw)]
+class Shell(Tools):
+    def get_tools(self, *a, **kw) -> list[BaseTool]:
+        return [ShellTool(*a, **kw)]
 
 
-def python_repl() -> list[BaseTool]:
-    @tool
-    def python_repl_tool(script):
-        """python repr run"""
-        return PythonREPL().run(script)
+class Python_(Tools):
+    def get_tools(self) -> list[BaseTool]:
+        @tool
+        def python_repl_tool(script):
+            """python repr run"""
+            return PythonREPL().run(script)
 
-    return [python_repl_tool]
-
-
-def wikipedia() -> list[BaseTool]:
-    return [WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())]
+        return [python_repl_tool]
 
 
-def serper() -> list[BaseTool]:
-    @tool
-    def serper_tool(query):
-        """serper search"""
-        return GoogleSerperAPIWrapper().run(query)
-
-    return [serper_tool]
+class Wikipedia(Tools):
+    def get_tools(self) -> list[BaseTool]:
+        return [WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())]
 
 
-def tavily() -> list[BaseTool]:
-    return [TavilySearch(max_results=5)]
+class Serper(Tools):
+    def get_tools(self) -> list[BaseTool]:
+        @tool
+        def serper_tool(query):
+            """serper search"""
+            return GoogleSerperAPIWrapper().run(query)
+
+        return [serper_tool]
 
 
-async def browser(browser) -> list[BaseTool]:
-    return PlayWrightBrowserToolkit.from_browser(async_browser=browser).get_tools()
+class Tavily(Tools):
+    def get_tools(self) -> list[BaseTool]:
+        return [TavilySearch(max_results=5)]
 
 
-def slack() -> list[BaseTool]:
-    return SlackToolkit().get_tools()
+class Browser(Tools):
+    async def get_tools(self, browser) -> list[BaseTool]:
+        return PlayWrightBrowserToolkit.from_browser(async_browser=browser).get_tools()
 
 
-def file_management(*a, **kw) -> list[BaseTool]:
-    return FileManagementToolkit(*a, **kw).get_tools()
+class FileManagement(Tools):
+    def get_tools(self, *a, **kw) -> list[BaseTool]:
+        return FileManagementToolkit(*a, **kw).get_tools()
 
 
-def sql(model, db_name) -> list[BaseTool]:
-    db_dir = Path(__file__).parent.parent.parent / "db"
-    path = db_dir / f"{db_name}.db"
-    path.parent.mkdir(parents=True, exist_ok=True)
-    conn = sqlite3.connect(path, check_same_thread=False)
-    engine = create_engine(
-        f"sqlite://{path}",
-        creator=lambda: conn,
-        poolclass=StaticPool,
-        connect_args={"check_same_thread": False},
-    )
-    db = SQLDatabase(engine)
-    return SQLDatabaseToolkit(db=db, llm=model).get_tools()
-
-
-# utils
-def make_tools_by_name(tools) -> dict[str, BaseTool]:
-    return {tool.name: tool for tool in tools}
+class SQL(Tools):
+    def get_tools(self, model, db_name) -> list[BaseTool]:
+        db_dir = Path(__file__).parent.parent.parent / "db"
+        path = db_dir / f"{db_name}.db"
+        path.parent.mkdir(parents=True, exist_ok=True)
+        conn = sqlite3.connect(path, check_same_thread=False)
+        engine = create_engine(
+            f"sqlite://{path}",
+            creator=lambda: conn,
+            poolclass=StaticPool,
+            connect_args={"check_same_thread": False},
+        )
+        db = SQLDatabase(engine)
+        return SQLDatabaseToolkit(db=db, llm=model).get_tools()
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -4,26 +4,22 @@ from langchain_core.tools.base import BaseToolkit
 from langgraph.prebuilt import create_react_agent
 
 from llm_experiments import tools
-from llm_experiments.custom_tools.slack import SlackToolkit
 from llm_experiments.llm import create_model
-from llm_experiments.tools import make_tools_by_name
 
 nest_asyncio.apply()
 
 
 def test_slack_tools():
-    slack_toolkit = SlackToolkit()
-    assert isinstance(slack_toolkit, BaseToolkit)
-    slack_tools = slack_toolkit.get_tools()
-    tool_by_name = make_tools_by_name(slack_tools)
-    res = tool_by_name["post_message"].invoke({"channel": "test", "text": "hello from agent"})
+    sl = tools.Slack()
+    assert isinstance(sl, BaseToolkit)
+    res = sl.get_tools_by_name()["post_message"].invoke({"channel": "test", "text": "hello from agent"})
     assert res["ok"]
-    res = tool_by_name["delete_message"].invoke({"channel": res["channel"], "ts": res["ts"]})
+    res = sl.get_tools_by_name()["delete_message"].invoke({"channel": res["channel"], "ts": res["ts"]})
     assert res["ok"]
 
 
 def test_duckduckgo_tools():
-    agent = create_react_agent(create_model(), [tools.duckduckgo()])
+    agent = create_react_agent(create_model(), tools.DuckDuckGo().get_tools())
     res = agent.invoke({"messages": "search today's news"})
     last_msg = res["messages"][-1].content
     assert last_msg
@@ -32,7 +28,7 @@ def test_duckduckgo_tools():
 
 async def test_browser():
     async with create_async_playwright_browser() as async_browser:
-        toolkit = await tools.browser_tools(async_browser)
+        toolkit = await tools.Browser().get_tools(async_browser)
         agent = create_react_agent(create_model(), toolkit)
         res = agent.invoke({"messages": "go to wikipedia.org"})
         last_msg = res["messages"][-1].content
@@ -41,7 +37,7 @@ async def test_browser():
 
 
 def test_shell_tools():
-    agent = create_react_agent(create_model(), [tools.shell()])
+    agent = create_react_agent(create_model(), tools.Shell().get_tools())
     res = agent.invoke({"messages": "ls"})
     for msg in res["messages"]:
         print(msg.content)
@@ -49,7 +45,7 @@ def test_shell_tools():
 
 
 def test_python_repl_tools():
-    agent = create_react_agent(create_model(), [tools.python_repl()])
+    agent = create_react_agent(create_model(), tools.Python_().get_tools())
     res = agent.invoke({"messages": "print('hello world')"})
     for msg in res["messages"]:
         print(msg.content)
@@ -57,7 +53,7 @@ def test_python_repl_tools():
 
 
 def test_wikipedia_tools():
-    agent = create_react_agent(create_model(), [tools.wikipedia()])
+    agent = create_react_agent(create_model(), tools.Wikipedia().get_tools())
     res = agent.invoke({"messages": "search for monty python"})
     for msg in res["messages"]:
         print(msg.content)
@@ -65,7 +61,7 @@ def test_wikipedia_tools():
 
 
 def test_file_management_tools():
-    agent = create_react_agent(create_model(), tools.file_management_tools())
+    agent = create_react_agent(create_model(), tools.FileManagement().get_tools())
     res = agent.invoke({"messages": "list files in current directory"})
     for msg in res["messages"]:
         print(msg.content)
@@ -73,7 +69,7 @@ def test_file_management_tools():
 
 
 def test_serper_tools():
-    agent = create_react_agent(create_model(), [tools.serper()])
+    agent = create_react_agent(create_model(), tools.Serper().get_tools())
     res = agent.invoke({"messages": "search for today's news"})
     for msg in res["messages"]:
         print(msg.content)
@@ -81,7 +77,7 @@ def test_serper_tools():
 
 
 def test_tavily_tools():
-    agent = create_react_agent(create_model(), [tools.tavily()])
+    agent = create_react_agent(create_model(), tools.Tavily().get_tools())
     res = agent.invoke({"messages": "search for today's news"})
     for msg in res["messages"]:
         print(msg.content)
@@ -90,7 +86,7 @@ def test_tavily_tools():
 
 def test_sql_tools():
     model = create_model()
-    agent = create_react_agent(model, tools.sql_tools(model, "test"))
+    agent = create_react_agent(model, tools.SQL().get_tools(model, "test"))
     res = agent.invoke({"messages": "select * from test"})
     for msg in res["messages"]:
         print(msg.content)
