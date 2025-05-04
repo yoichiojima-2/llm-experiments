98dea38f7ac73128b799b40553481524e8037748
yoichi ojima <yoichiojima@gmail.com>
2025-05-04 17:51:52 +0900

    fix: change MCP server interface

--- src/llm_experiments/server.py
+++ src/llm_experiments/server.py
@@ -1,60 +1,51 @@
 from fastmcp import FastMCP
-from langchain_core.tools.base import BaseTool
-from langgraph.checkpoint.memory import MemorySaver
 
-from llm_experiments import prebuilt
 from llm_experiments import tools as t
-from llm_experiments.llm import create_model
 
 
 class MCPServer:
-    def __init__(self, name: str, tools: list[BaseTool] = [], tags: list[str] = []):
-        self.mcp = FastMCP(name)
+    def __init__(self, name, tools: list[t.Tools] = None, tags=[]):
+        self.server = FastMCP(name)
         self.name = name
         self.tools = tools
         self.tags = [name, *tags]
         self.build()
 
-    def build(self) -> FastMCP:
-        for tool in self.tools:
-            self.mcp.add_tool(tool.func, name=tool.name, description=tool.description, tags=self.tags)
+    def composite(self, server):
+        self.server.mount(server.name, server.server)
 
-    def serve(self):
-        self.mcp.run()
+    def build(self):
+        if self.tools:
+            for tools in self.tools:
+                for tool in tools.get_tools():
+                    self.server.add_tool(tool.func, name=tool.name, description=tool.description, tags=self.tags)
 
-    def mount(self, server: FastMCP):
-        self.mcp.mount(server.name, server.mcp)
-
-
-def build_dev() -> MCPServer:
-    return MCPServer(name="dev", tools=[*t.Shell().get_tools(), *t.Python_().get_tools()], tags=["dev"])
+    def run(self):
+        self.server.run()
 
 
-def build_research() -> MCPServer:
+def dev():
+    return MCPServer(name="dev", tools=[t.Shell(), t.Python_()], tags=["dev"])
+
+
+def research():
     return MCPServer(
         name="research",
-        tools=[
-            *t.Wikipedia().get_tools(),
-            *t.DuckDuckGo().get_tools(),
-            *t.Tavily().get_tools(),
-            *t.Serper().get_tools(),
-        ],
+        tools=[t.Wikipedia(), t.DuckDuckGo(), t.Tavily(), t.Serper()],
         tags=["research"],
     )
 
 
-def build_slack() -> MCPServer:
-    return MCPServer(name="slack", tools=[*t.Slack().get_tools()], tags=["slack"])
+def slack():
+    return MCPServer(name="slack", tools=[t.Slack()], tags=["slack"])
 
 
 def main():
     main = MCPServer("llm_experiments")
-
-    children = [build_dev(), build_research(), build_slack()]
+    children = [dev(), research(), slack()]
     for child in children:
-        main.mount(child)
-
-    main.serve()
+        main.composite(child)
+    main.run()
 
 
 if __name__ == "__main__":
